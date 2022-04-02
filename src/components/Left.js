bcd7e164b61501b3fc2b77637f90352c21e3f696
mrshanas <nassibshaban345@gmail.com>
2022-04-02 22:24:25 +0300

    develop: content

---
+++ src/components/Left.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Left = () => {
+  return (
+    <div className="app__left">
+      <div className="app__heading">
+        <h1>Learn to code by watching others</h1>
+      </div>
+      <div className="app__text">
+        <p>
+          See how experienced developers solve problems in real time.
+          <br />
+          Watching scripted tutorials is great, but understanding how developers
+          think is invaluable
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Left;
---
+++ src/components/Right.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Right = () => {
+  return (
+    <div className="app__right">
+      <div className="app__button">
+        <button>Try it free 7 days then $20/mo thereafter</button>
+      </div>
+      <div className="app__form">
+        <form>
+          <input type="text" name="firstname" placeholder="First Name" />
+          <br />
+          <input type="text" name="lastname" placeholder="Last Name" />
+          <br />
+          <input type="email" name="email" placeholder="Email Address" />
+          <br />
+          <input type="password" name="password" placeholder="Password" />
+          <br />
+          <button type="submit">CLAIM YOUR FREE TRIAL</button>
+        </form>
+        <p>
+          By clicking this button you are agreeing to our{" "}
+          <a href="#">Terms and services</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Right;
